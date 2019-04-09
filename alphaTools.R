48139a611a494f86a81fe0fb2bc4ea7a26c862c4
Nicholas Santantonio <ns722@cornell.edu>
2019-04-09 17:56:44 -0400

    simPlot can cal varietiy means if not done during getPopStats

--- alphaTools.R
+++ alphaTools.R
@@ -186,27 +186,6 @@ getPopStats <- function(resultL, meanVariety = FALSE){
 }
 
 
-
-# getPopStats <- function(pop, Ryr = RGSCyr, meanVariety = TRUE){
-#     VgRGSC <- sapply(pop[["RGSC"]], genicVarG)
-#     gvRGSC <- sapply(pop[["RGSC"]], function(x) mean(gv(x)))
-#     # gvVariety <- sapply(run1[["VDP"]][["variety"]], function(x) mean(gv(x)))
-#     gvVariety <- lapply(pop[["VDP"]][["variety"]], function(x) gv(x))
-#     SDgRGSC <- sqrt(VgRGSC)
-   
-#     # meanVariety <- TRUE
-#     if(meanVariety){
-#       Yvariety <- sapply(gvVariety, mean)
-#       Xvariety <- Ryr[1:length(Yvariety)]
-#     } else {
-#       nVariety <- sapply(gvVariety, nrow)
-#       Yvariety <- unlist(gvVariety)
-#       Xvariety <- rep(Ryr[1:length(nVariety)], times = nVariety)
-#     }
-
-#     return(list(Vg = VgRGSC, gv = gvRGSC, sd = SDgRGSC, vx = Xvariety, vy = Yvariety))
-# }
-
 getYrange <- function(simR) range(c(simR$gv + simR$sd, simR$gv - simR$sd, simR$vy))
 
 invertList <-  function(ll) {
@@ -230,25 +209,24 @@ plotPop <- function(simL, Rgen = RGSCgen, vLine = FALSE, popcol = "#000000", alp
   }
 
 
-# baseCols = c("#00FF00", "#0000FF")
-simPlot <- function(popList, baseCols = "#000000", popLabs = NULL, varLine = TRUE){
-    # if(class(popList[[1]][[1]][[1]]) == "Pop") popList <- list(popList) # checks that popList is a list of pop
-    
+simPlot <- function(popList, baseCols = "#000000", popLabs = NULL, varLine = TRUE, meanVariety = TRUE, legendPos = "topleft"){
+    avgInGen <- function(x, xname) {
+		x
+    	ly <- length(x)
+    	lapply(x, function(xx, l = lx) tapply(xx, rep(1:(length(xx)/lx), each = lx), mean))
+	}
+
     if(length(baseCols) != length(popList)) stop("baseCols must be same length as popList!")
     lineCol <- paste0(baseCols, "FF")
     ptCol <- paste0(baseCols, "FF")
     polyCol <- paste0(baseCols, "4D")
 
     if(is.null(popLabs)) popLabs <- names(popList)
-    # yr <- 1:nYr
-    # RGSCyr <- yr * GScylcePerYr
-    # RGSCgen <- c(0, 1:(nYr * GScylcePerYr))
-    # simStats <- rlapply(popList, f=getPopStats, level = 2, Ryr = RGSCyr)
 
-    # GScylcePerYr <- 2
     GScylcePerYr <- popList[[1]][[1]][["paramL"]][["GScylcePerYr"]]
     simStats <- lapply(popList, function(x) lapply(x, function(xx) xx[!names(xx) %in% c("SP", "paramL")]))
     simStatsInv <- lapply(simStats, invertList)
+    if(meanVariety) for(i in 1:length(simStatsInv)) simStatsInv[[i]][c("vy", "vx")] <- lapply(simStatsInv[[i]][c("vy", "vx")], avgInGen)
     simReps <- rlapply(simStatsInv, level = 3, combine = rbind) 
     simAvg <- rlapply(simReps, f = colMeans, level = 2, na.rm = TRUE)
 
@@ -262,15 +240,14 @@ simPlot <- function(popList, baseCols = "#000000", popLabs = NULL, varLine = TRU
     axis(1, at = c(0, RGSCyr), labels = c(0, yr))
 
     for(i in 1:length(popList)){
-      invisible(lapply(simStats[[i]], plotPop, Rgen = RGSCgen, popcol = baseCols[i]))
-      plotPop(simAvg[[i]], popcol = baseCols[i], alpha = "FF", alphaMean = "4D", Rgen = RGSCgen, vLine = varLine)
+    	invisible(lapply(simStats[[i]], plotPop, Rgen = RGSCgen, popcol = baseCols[i]))
+    	plotPop(simAvg[[i]], popcol = baseCols[i], alpha = "FF", alphaMean = "4D", Rgen = RGSCgen, vLine = varLine)
     }
 
     if(length(popList) > 1) {
-        legend("topright", legend = popLabs, col=baseCols, lty = 1)
+        legend(legendPos, legend = popLabs, col=baseCols, lty = 1)
       } else {
-        legend("topright", legend = c("RGSC mean", expression(paste('RGSC ', sigma[g])), "Variety mean"), 
-         # fill = c(NA, polyCol, NA), 
+        legend(legendPos, legend = c("RGSC mean", expression(paste('RGSC ', sigma[g])), "Variety mean"), 
          bty = "n",
          col = c(lineCol, "black", ptCol),
          lty = c(1, 0, 0), lwd = c(2, 0, 0),
@@ -279,65 +256,4 @@ simPlot <- function(popList, baseCols = "#000000", popLabs = NULL, varLine = TRU
         )
 	}
 }
-
-
-# simPlot <- function(popList, baseCols = "#000000", popLabs = NULL, varLine = TRUE){
-#     if(class(popList[[1]][[1]][[1]]) == "Pop") popList <- list(popList) # checks that popList is a list of pop
-    
-#     if(length(baseCols) != length(popList)) stop("baseCols must be same length as popList!")
-#     lineCol <- paste0(baseCols, "FF")
-#     ptCol <- paste0(baseCols, "FF")
-#     polyCol <- paste0(baseCols, "4D")
-
-#     if(!is.null(popLabs)) popLabs <- names(popList)
-#     yr <- 1:nYr
-#     RGSCyr <- yr * GScylcePerYr
-#     RGSCgen <- c(0, 1:(nYr * GScylcePerYr))
-    
-#     simStats <- rlapply(popList, f=getPopStats, level = 2, Ryr = RGSCyr)
-#     simStatsInv <- lapply(simStats, invertList)
-
-#     simReps <- rlapply(simStatsInv, level = 3, combine = rbind) 
-#     simAvg <- rlapply(simReps, f = colMeans, level = 2)
-
-#     xlims <- range(c(0, RGSCgen))
-#     ylims <- range(sapply(simReps, getYrange)) * 1.1
-
-#     plot(NA, xlim = xlims, ylim = ylims, xaxt = "n", xlab = "generation", ylab = "standardized genetic value")
-#     axis(1, at = c(0, RGSCyr), labels = c(0, yr))
-
-#     for(i in 1:length(popList)){
-#       invisible(lapply(simStats[[i]], plotPop, Rgen = RGSCgen))
-#       plotPop(simAvg[[i]], popcol = baseCols[i], alpha = "FF", alphaMean = "4D", Rgen = RGSCgen, vLine = varLine)
-#     }
-
-#     if(length(popList) > 1) {
-#         legend("topright", legend = popLabs, col=baseCols, lty = 1)
-#       } else {
-#         legend("topright", legend = c("RGSC mean", expression(paste('RGSC ', sigma[g])), "Variety mean"), 
-#          # fill = c(NA, polyCol, NA), 
-#          bty = "n",
-#          col = c(lineCol, "black", ptCol),
-#          lty = c(1, 0, 0), lwd = c(2, 0, 0),
-#          pch = c(NA, 22, 16),
-#          pt.bg = c(NA, polyCol, ptCol),
-#         )
-#   }
-# }
-
-# # popList <- list(simrun, simrun)
-# popList <- list(simrun)
-
-
-
-
-# sel <- if(RGSCselect) "sel" else "nosel" 
-# f <- if(selF2) "F2" else "F1" 
-
-# source("testparam.R")
-# # pdf("RGSCselF2.pdf", width = 10, height = 7)
-# pdf(paste0("RGSC", sel, f, ".pdf"), width = 10, height = 7)
-# # simPlot(popList, c("#00FF00", "#0000FF"))
-# simPlot(popList)
-# dev.off()
 
