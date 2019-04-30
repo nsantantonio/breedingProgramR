5da5dd75f577aca5bd7cab29e616c94aa68ffa4b
Nicholas Santantonio <ns722@cornell.edu>
2019-04-30 09:57:15 -0400

    restored defaults to singleTraitProgram

--- singleTraitProgram.R
+++ singleTraitProgram.R
@@ -31,16 +31,16 @@ defArgs <- list(
 	selectVDP = "pheno", # ebv, pheno
 	returnVDPcrit = "pheno", # ebv?
 	selFuncOut = NULL, # truncSel, expDist, simDHdist
-	selFuncIn = simDHdistPairs, # truncCross, expDistPairs, simDHdistPairs, maxVar
+	selFuncIn = NULL, # truncCross, expDistPairs, simDHdistPairs, maxVar
 	withinFamInt = 1, #  
 	setXint = NULL, # note that x is the cdf of a normal 
 	skip = NULL,
 	weight = 0.5,
 # family parameters
 	nFounder = 10,
-	nNuclear = 10,
+	nNuclear = 100,
 	nFam = 10,
-	famSize = 20,
+	famSize = 50,
 	ssd = FALSE,
 	selF2 = FALSE,
 	nF2 = 1,
