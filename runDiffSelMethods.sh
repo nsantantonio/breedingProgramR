f0aad319611e1ab5b71b178466055ebd89c67d50
Nicholas Santantonio <ns722@cornell.edu>
2019-04-30 14:29:15 -0400

    run diff methods, default

--- runDiffSelMethods.sh
+++ runDiffSelMethods.sh
@@ -18,17 +18,19 @@ for i in truncSel expDist simDHdist; do
     done
 done
 
-for i in truncSel expDist simDHdist; do
-    for j in expDistPairs simDHdistPairs maxVar; do 
-        args=("seed=12345" \
-            "simName=${i}_${j}_noTrunc" \
-            "selFuncOut=(${i})" \
-            "selFuncIn=(${j})"\
-            "selectRGSC=1")
-        # echo ${args[1]} ${args[2]}
-        Rscript ${pardir}/singleTraitProgram.R --args "${args[@]}" > ${pardir}/logs/log_${i}_${j}.txt &
-    done
-done
+# for i in truncSel expDist simDHdist; do
+#     for j in expDistPairs simDHdistPairs maxVar; do 
+#         args=("seed=12345" \
+#             "simName=${i}_${j}_noTrunc" \
+#             "selFuncOut=(${i})" \
+#             "selFuncIn=(${j})"\
+#             "selectRGSC=1")
+#         # echo ${args[1]} ${args[2]}
+#         Rscript ${pardir}/singleTraitProgram.R --args "${args[@]}" > ${pardir}/logs/log_${i}_${j}_noTrunc.txt &
+#     done
+# done
+
+
 
 # Note: functions, NULL or anything else that needs to be evaluated must be put in parentheses so they are evaluated properly, otherwise they will be treated as strings
 # for i in truncSel expDist simDHdist; do
